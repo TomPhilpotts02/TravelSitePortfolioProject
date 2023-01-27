f81d2a1aa4bb75b4a32a5c4f53d30df501bd1ac8
Tom Philpotts <104871668+TomPhilpotts02@users.noreply.github.com>
2023-01-27 22:00:25 +0000

    Add files via upload

---
+++ TravelSitePortfolioProject/script.js
@@ -0,0 +1,12 @@
+const test = () => {
+    console.log('worked!');
+}
+
+window.onload = test();
+
+const propertyListings = [
+    {id: 1, location: 'UK', guests: 6, rating: 3.2, name: 'Uk Property 1', desc: 'A Lovely Property In The Heart of England!'},
+    {id: 1, location: 'Sweden', guests: 2, rating: 4.0, name: 'Sweden Property 1', desc: 'A Lovely Property In The Heart of Sweden!'},
+    {id: 1, location: 'Italy', guests: 4, rating: 1.2, name: 'Italy Property 1', desc: 'A Lovely Property In The Heart of Italy!'},
+    {id: 1, location: 'UK', guests: 12, rating: 5.0, name: 'Uk Property 2', desc: 'A Lovely Property In The Heart of England!'},
+]
